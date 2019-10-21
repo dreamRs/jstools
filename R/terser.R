18710acd4560670eaaf56dbeb06d9e86a9c25dbb
pvictor <perrier.victor@gmail.com>
2019-10-21 22:18:55 +0200

    terser work with several files

--- R/terser.R
+++ R/terser.R
@@ -15,16 +15,24 @@
 #' @example examples/ex-terser.R
 terser_file <- function(input, options = terser_options(), output = NULL) {
   input <- normalizePath(path = input, mustWork = TRUE)
-  input <- readLines(con = input, encoding = "UTF-8")
-  result <- terser(code = input, options = options)
+  if (length(input) == 1) {
+    inputs <- readLines(con = input, encoding = "UTF-8")
+  } else {
+    inputs <- lapply(input, readLines, encoding = "UTF-8")
+    names(inputs) <- basename(input)
+  }
+  result <- terser(code = inputs, options = options)
   if (!is.null(result$error)) {
     message(result$error$name, result$error$message)
+    return(invisible(result))
   } else {
     if (!is.null(output)) {
       writeLines(text = result$code, con = output)
+      return(invisible(result))
+    } else {
+      return(result)
     }
   }
-  return(invisible(result))
 }
 
 #' @param code Character vector where each element represent a line of JavaScript code,
@@ -39,7 +47,12 @@ terser <- function(code, options = terser_options()) {
   ctx <- v8()
   ctx$source(file = system.file("assets/terser/source-map.min.js", package = "jstools"))
   ctx$source(file = system.file("assets/terser/bundle.min.js", package = "jstools"))
-  ctx$assign("code", paste(code, collapse = "\n"))
+  if (is.list(code)) {
+    code <- lapply(code, paste, collapse = "\n")
+  } else {
+    code <- paste(code, collapse = "\n")
+  }
+  ctx$assign("code", code)
   ctx$assign("options", options)
   ctx$eval("var result = Terser.minify(code, options);")
   ctx$get("result")
--- examples/ex-terser.R
+++ examples/ex-terser.R
@@ -1,7 +1,15 @@
 
 terser("function add(first, second) { return first + second; }")
 
+# Mimic several files
+terser(list(
+  "file1.js" = "function add(first, second) { return first + second; }",
+  "file2.js" = "add(1 + 2, 3 + 4);"
+))
+# equivalent to:
+#> terser_file(c("file1.js", "file2.js"))
 
+# With a file
 path <- system.file("testfiles/htmlwidgets.js", package = "jstools")
 terser_file(input = path)
 
